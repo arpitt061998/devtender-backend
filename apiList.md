9ee54cdf3873b6907375776a35a85f974a9b5f06
Arpit Tiwari <arpittiwari@Arpits-MacBook-Air.local>
2025-05-08 02:09:17 +0530

    adding feed api and pagination support on feed apis

--- apiList.md
+++ apiList.md
@@ -9,7 +9,6 @@ authRouter
 - PATCH /profile/edit
 - PATCH /profile/password
 
-
 # STATUS: ignore, interested, accepted, rejected
 connectionRequestRouter
 - POST /request/send/interested/:userId
@@ -19,8 +18,14 @@ connectionRequestRouter
 
 # userrouter
 - GET /user/connections
-- GET user//requests
-- GET /feed - gets you the profile of other users on platform
+- GET /user/requests
+- GET /user/feed - gets you the profile of other users on platform
+
+- pagination
+/ feed?page=1&limit=10 => 1-10  => mongo .skip(0).limit(10)
+/ feed?page=2&limit=10 => 11-20 => mongo .skip(0).limit(10)
+
+
 
 
 
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -3,7 +3,7 @@ import { AuthRequest } from "../types/interface";
 import ConnectionRequest from "../model/connectionRequest";
 import userAuth from "../middleware/auth";
 const userRouter = express.Router();
-import { IUser } from "../model/user";
+import User, { IUser } from "../model/user";
 
 const USER_SAFE_DATA = ["firstName", "lastName", "age", "skills", "gender", "photoUrl"];
 userRouter.get("/user/requests/pending", userAuth, async (req: AuthRequest, res: Response) => {
@@ -66,6 +66,73 @@ userRouter.get("/user/connections", userAuth, async(req: AuthRequest, res: Respo
         });
     } catch(err: any) {
         res.status(500).json({ message: "Internal server error "+ err.message });
+    }
+});
+
+userRouter.get("/user/feed",userAuth, async(req: AuthRequest, res: Response) => {
+    try{
+        const loggedInUser = req.user;
+        const userId = loggedInUser?._id;
+        const page = parseInt(req.query.page as string || "1");
+        let limit = parseInt(req.query.limit as string || "10");
+
+        limit = limit > 50 ? 50 : limit;
+        const skip = (page-1)*limit;
+
+        // User should see all the user card expect-
+        // his own card
+        // ignored people
+        // already sent connection request
+
+        const connectionRequests = await ConnectionRequest.find({
+            $or: [
+                {fromUserId: userId},
+                {toUserId: userId}
+            ]
+        }).select("fromUserId toUserId");
+
+        const hideUserIds = new Set<string>();
+
+        connectionRequests.forEach((connection) => {
+            hideUserIds.add(connection.fromUserId.toString());
+            hideUserIds.add(connection.toUserId.toString());
+        })
+
+        const feedUsers = await User.find({
+            _id: {$nin: Array.from(hideUserIds)}
+        }).select(USER_SAFE_DATA).skip(skip).limit(limit);
+
+        res.status(200).send({
+            message: "Feeds are: ",
+            feed: feedUsers
+        });
+    } catch(err:any) {
+        res.status(400).send("Error: "+err)
+    }
+});
+
+//api to list all users present expect loggedinuser ~ just for fun
+userRouter.get("/user/all", userAuth, async(req: AuthRequest, res: Response) => {
+    try {
+        const loggedInUser = req.user;
+        const userId = loggedInUser?._id;
+        const page = parseInt(req.query.page as string || "1");
+        const limit = parseInt(req.query.limit as string || "10");
+        const skip = (page-1)*limit;
+
+        const users = await User.find({
+            _id: {$ne: userId}
+        }).skip(skip).limit(limit);
+        const count = await User.countDocuments({
+            _id: { $ne: userId }
+          });
+        
+        res.status(200).send({
+            message: "Total users are " + (count+1),
+            users: users
+        })
+    } catch(err) {
+        res.status(400).send("Failed")
     }
 });
 
