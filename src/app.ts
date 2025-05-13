bf01e2c3cea0a93b0ac251fc8273c6cf1b0a50a9
arpitt061998 <arpittiwari9898@gmail.com>
2025-05-14 00:04:09 +0530

    adding cors package

--- src/app.ts
+++ src/app.ts
@@ -5,13 +5,18 @@ import profileRouter from './routes/profile';
 import authRouter from './routes/auth';
 import requestRouter from './routes/request';
 import userRouter from './routes/user';
+import cors from 'cors';
 const app = express();
 const port = 3000;
 const SALT_ROUNDS = 10;
 
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH" ,"DELETE"],
+}));
 app.use(express.json());
 app.use(cookieParser());
-
 app.use("/",authRouter);
 app.use("/",profileRouter);
 app.use("/",requestRouter);
